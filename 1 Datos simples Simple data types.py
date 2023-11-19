c98337147be1fe87dc9163571862e9a0f7293769
GCSLAYER <jc_sr31@outlook.com>
2023-11-18 23:07:41 -0600

    first hour of course from channel dalto

---
+++ 1 Datos simples Simple data types.py
@@ -0,0 +1,18 @@
+"string"
+'este es un string de gato'
+
+"""" los tipos de gato:
+    angora
+    egipcio"""
+    
+''' los tipos de gato:
+    angora
+    egipcio'''
+    
+"Integer" 
+80
+"Float"
+80.5
+"Boolean"
+False
+True
---
+++ 2 Variables.py
@@ -0,0 +1,36 @@
+a = 3
+b = 6
+c = a + b
+
+print (c)
+
+#Declaration variable -  Definition Variable
+name = "Dam"
+name = "Ian"
+name = "Kerjoy"
+
+#print (name)
+
+#add
+number = 5
+number = 10 + 2
+
+#print (number)
+
+#sintaxis for a = a + n equal to a += n
+
+number = 5
+number += 10
+
+print (number)
+
+#concatenate python strings 
+
+msg = "cat" + "dog" + " nice " + name
+print(msg)
+
+#fstrings can add a variable and convert to string
+print(f"Hi {name} how are you?")
+
+
+
