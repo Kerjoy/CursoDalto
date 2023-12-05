1ecb61a84bc10f18b60a0ce9f0bad0b6c1b6f31e
GCSLAYER <jc_sr31@outlook.com>
2023-12-05 16:09:52 -0600

    end of composite data

--- 3 datos compuestos.py
+++ 3 datos compuestos.py
@@ -1,6 +1,35 @@
+#
 #can modify
-list = ["Juan","Ker",True,1.85]
+#you must put in parentheses []
+list = ["Juan","Ker",True,1.73]
 
 #dif between list, is cant modify
-tupla = ["Juan","Ker",True,1.85]
-print(list[0])
+#you must put in parentheses ()
+tuple = ("Juan","Ker",True,1.73)
+
+
+#correct
+list[2] = "Taquito"
+
+#incorrect
+#tupla[2] = "Taquito"
+
+#print(list[2])
+
+#creating a set
+#you must put in parentheses {}
+#can modify but not have access from index
+#set do not allow duplicates
+set = {"name", "last name", True, 1.73}
+
+#creating a dictionary (dict)
+#dictionary can use a key to get value of any storage variable
+# the structure is key : value and comma-separated, and repeat, the last items no longer need a comma
+dictionary = {
+    'name' : "Ker",
+    'pet' : "cat",
+    'bool' : True,
+    'age' : 27
+}
+
+print (dictionary['age'])
