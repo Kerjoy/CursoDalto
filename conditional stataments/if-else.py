5b6458ed05936bad9fa07699d7a821912a073de1
GCSLAYER <jc_sr31@outlook.com>
2023-12-07 16:37:13 -0600

    adding if-else

---
+++ conditional stataments/if-else.py
@@ -0,0 +1,21 @@
+age = 17
+
+if age >= 18:
+    print ("access granted")
+    print ("block if")
+else:
+    print("access denied")
+    print("block else")
+    
+    
+print ("end of program block")
+
+password = "SecurePass"
+written_pass = "CrackPass"
+
+if password == written_pass:
+    print("correct password")
+else:
+    print("wrong password")
+    
+    
