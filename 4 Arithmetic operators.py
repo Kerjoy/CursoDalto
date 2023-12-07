d96ba39762059485b5153608b92ca3969255690a
GCSLAYER <jc_sr31@outlook.com>
2023-12-06 19:41:36 -0600

    ranaming and operator

---
+++ 4 Arithmetic operators.py
@@ -0,0 +1,29 @@
+""""
+Arithmetic operators
++ add
+- subtraction
+* multiplication or product
+/ division
+% module - returns the remainder from division
+** raise to the power
+// floor division - returns the integer part of the division
+"""
+
+addx = 12 + 5
+subx = 8 - 6
+
+multix = 12 * 5
+divx = 12 / 5
+
+mod = 12%5
+
+power = 14 ** 5
+
+floordiv = 23//4
+
+#function return the type of the variable
+data_type = type(floordiv)
+
+print (addx," ",subx," ",multix," ",divx," ",mod," ",power," ",floordiv)
+
+print (data_type)
