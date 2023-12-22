9ba1fd5322660b2f5e8298c888233bf0325a8dc8
GCSLAYER <jc_sr31@outlook.com>
2023-12-21 22:27:01 -0600

    list methods

--- methods/10 list methods.py
+++ methods/10 list methods.py
@@ -29,12 +29,13 @@ print(list1)
 list1.insert(0,"michi")
 print(list1)
 
-#EXTEND - adds some elements to a list
+#EXTEND - adds an iterable
 list1.extend(list1)
 print(list1)
 
 #POP - deletes an element from a list, requires an index and returns a value
 print(list1.pop(0))
+print(list1.pop(-1))#deletes last element from the list -2 prev last and so on
 
 #REMOVE - removes an element from a list, specifying a value
 list1.remove("here")
@@ -45,4 +46,13 @@ list1.clear()
 print(list1)
 
 #SORT - sorts a list in ascending or descending order
-#REVERSE - reverses the order of elements in a list
+number_list = ([2,3,5,6,8,0,102,24,45,123, True, True, False, False]) # dont work with strings
+number_list.sort()
+print(f"Demostration of sort method for a list {number_list}")
+number_list.sort(reverse=True)
+print(f"Demostration of sort method with attribute reverse for a list {number_list}")
+
+#REVERSE - reverses the order of elements in a list without sort
+number_list.reverse()
+print(f"reversing a list {number_list}")
+
