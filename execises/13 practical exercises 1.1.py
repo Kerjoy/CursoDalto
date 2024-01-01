53127df129f39c76c428fe53c4063ddd4d77ce44
GCSLAYER <jc_sr31@outlook.com>
2023-12-31 23:51:44 -0600

    percent dalto exercise

---
+++ execises/13 practical exercises 1.1.py
@@ -0,0 +1,24 @@
+"""
+exercise 1
+
+a- difference between in percent the actual course
+the fastest of other courses
+the slowes of other courses
+the average of all courses
+
+b- percent of unusable material that was reduced on:
+the average of all courses
+the actual course
+
+c- watch 10 hours of this course and vice versa
+
+"""
+
+
+dalto_course_time = 1.5
+minimal_course_time = 2.5
+average_course_timee = 4.0
+longest_course_time = 7.0
+
+fastest_dif = 0
+print(fastest_dif)
