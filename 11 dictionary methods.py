8fe584087cdd77dc26d610b9caf9351630c3cc4e
GCSLAYER <jc_sr31@outlook.com>
2023-12-31 23:24:53 -0600

    finalizacion de data inputs

--- 11 dictionary methods.py
+++ 11 dictionary methods.py
@@ -1,5 +1,5 @@
 """
-KEYS() -> return the keys (useful for iteration)
+keys() -> return the keys (useful for iteration)
 get() -> return the value of a key
 clear() -> deletes all elements
 pop() -> deletes a element
@@ -12,9 +12,21 @@ dictionary_1 = {
     "prices" : '10'
 }
 
+#return a object dict_item
 keys_dict = dictionary_1.keys()
-
 print(keys_dict)
 
-for keys_dict in dictionary_1:
-    print(keys_dict)
+for keys_dict in dictionary_1: # iteration on dictionary
+    print(keys_dict) #print keys 
+    print(dictionary_1.get(keys_dict)) #print fields of dictionary, dont rise exceptions
+    
+#deleting all dictionary
+#dictionary_1.clear()
+
+#deleting elements of dictionary with key value, dictionary_1.pop("name","material")
+dictionary_1.pop("name")
+
+#getting a element dict_items iterable
+dictionaru_iter = dictionary_1.items()
+
+print(dictionary_1)
---
+++ 12 data inputs.py
@@ -0,0 +1,7 @@
+#ask data to user
+
+name = input("introduce a user name: ")
+print(f'el nombre es: {name}')
+
+#all data in a input is a string type for convert use a function as int() float()
+
