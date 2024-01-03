1183668147aed2866f12f1dc385e6f33bd924119
GCSLAYER <jc_sr31@outlook.com>
2024-01-02 22:18:26 -0600

    practical exercises

--- execises/13 practical exercises 1.1.py
+++ execises/13 practical exercises 1.1.py
@@ -3,7 +3,7 @@ exercise 1
 
 a- difference between in percent the actual course
 the fastest of other courses
-the slowes of other courses
+the slowest of other courses
 the average of all courses
 
 b- percent of unusable material that was reduced on:
@@ -17,8 +17,14 @@ c- watch 10 hours of this course and vice versa
 
 dalto_course_time = 1.5
 minimal_course_time = 2.5
-average_course_timee = 4.0
+average_course_time = 4.0
 longest_course_time = 7.0
 
-fastest_dif = 0
-print(fastest_dif)
+fastest_dif = 100 - dalto_course_time / minimal_course_time * 100
+slowest_dif = 100 - dalto_course_time * 1000 // longest_course_time / 10 #a way for controlling decimal numbers
+average_dif = 100 - dalto_course_time / average_course_time * 100
+
+
+print(f"the dalto course have duration {fastest_dif} % more fast in comparison than minimal")
+print(f"the dalto course have duration {slowest_dif} % more fast in comparison than longest")
+print(f"the dalto course have duration {average_dif} % more fast in comparison than average")
