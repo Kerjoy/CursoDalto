e7dae9cf5dc70080d45ded40b776d0dc958a3c5e
GCSLAYER <jc_sr31@outlook.com>
2024-02-08 21:42:24 -0600

    accesing to first rows and last

--- files/36 read_csv.py
+++ files/36 read_csv.py
@@ -15,9 +15,13 @@ file_descendent = data_frame.sort_values("randomt3", ascending=False)
 #print(file_descendent)
 
 
-data_frame_concat = pd.concat([file_ascendent,file_descendent])
+data_frame_concat = pd.concat([file_ascendent,file_descendent]) #concat the data drames
 
-print(data_frame_concat)
+first_row = data_frame.head(2) #aceesing to first 2 rows
+
+last_row = data_frame.tail(2) #accesinf to last 2 rows
+
+print(last_row)
 
 #getting values of column tech1
 #t1 = data_frame["Tech1"]
